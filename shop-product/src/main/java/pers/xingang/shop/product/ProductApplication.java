50015df2424b644a2877823d2db77709361f84bf
xingang <findxingang@163.com>
2024-04-10 16:17:39 +0800

    初始化项目

---
+++ shop-product/src/main/java/pers/xingang/shop/product/ProductApplication.java
@@ -0,0 +1,39 @@
+package pers.xingang.shop.product;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+
+/**
+ * @author xingang
+ * @since 2024/04/10 15:04
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(ProductApplication.class, args);
+    }
+
+    @Bean
+    @Order
+    CommandLineRunner commandLineRunner(Environment environment) {
+        return args -> {
+            String appName = environment.getProperty("spring.application.name") != null ? environment.getProperty("spring.application.name") : "";
+            String port = environment.getProperty("server.port") != null ? environment.getProperty("server.port") : "8080";
+            String path = environment.getProperty("server.servlet.context-path") != null ? environment.getProperty("server.servlet.context-path") : "";
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            System.out.println(
+                    "\n\n\t" +
+                            "----------------------------------------------------------\n\t" +
+                            "Application " + appName +" is running! Access URLs:\n\t" +
+                            "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                            "External: \thttp://" + ip + ":" + port + path + "/\n\n\t" +
+                            "------------------------------------------------------------"
+            );
+        };
+    }
+}
